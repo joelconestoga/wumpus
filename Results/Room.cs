111264277c2d3968653a4c1a82e06a863883e303
shubham0677 <shubham.sharma0677@gmail.com>
2017-03-23 20:21:44 -0400

    Added supply room feature

--- Results/Room.cs
+++ Results/Room.cs
@@ -4,8 +4,11 @@ namespace WumpusGame
 {
     public class Room : Result
     {
+        public static bool hasSupply = true;
+
         public int Number { get; set; }
         public string Sign { get; set; }
+        public bool Supply { get; set; }
         public Room FrontRoom { get; internal set; }
         public Room LeftRoom { get; internal set; }
         public Room RightRoom { get; internal set; }
@@ -15,6 +18,7 @@ namespace WumpusGame
         {
             this.Number = number;
             this.Sign = sign;
+            this.Supply = false;
             Trap = new TrapFree();
         }
 
@@ -28,8 +32,20 @@ namespace WumpusGame
             return Trap.getShot();
         }
 
-        public Result getIn()
+        public Result getIn(Warrior warrior)
         {
+            if(this.Supply)
+            {
+                Console.WriteLine("\n\nYou step into the Supply Room. You get more arrows! Yay!");
+                Console.WriteLine("(P.S The Supply is over now)");
+                Console.WriteLine("Press any key to continue...");
+                warrior.Arrows += 3;
+                this.Supply = false;
+                Room.hasSupply = false;
+                Console.ReadKey();
+                return this;
+            }
+
             Result result = Trap.getIn();
             return result == null ? this : result;
         }
